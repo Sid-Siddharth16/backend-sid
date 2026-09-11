cf3e8569fa4bfb129de046f0515d26a55cf7119e
Siddharth Pandey <siddharth.pandey@appinventiv.com>
2026-09-11 15:52:30 +0530

    db connection mongo

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// require('dotenv').config()
+import dotenv from "dotenv";
+import connectToDatabase from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+})
+
+
+
+connectToDatabase()
+
+
+/*
+import express from "express";
+
+const app = express();
+
+; (async () => {
+
+    try {
+
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.error("Error connecting to MongoDB", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`APP is running on port ${process.env.PORT}`);
+        });
+
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+        throw err;
+    }
+
+})()
+
+*/
